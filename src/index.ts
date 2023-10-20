7803e884e37d710422789f2aa189f518061c6a3d
JoseAngelCabanes <joseangelcabanes29@gmail.com>
2023-10-20 10:14:52 +0200

    feat: connection to database with typeORM

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express from "express";
 
 import { router } from "./routes/usersRoutes";
+import { AppDataSource } from "./db";
 
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
-// middleware
+//middleware
+
 app.use(express.json())
 
 app.use('/users', router)
+
+AppDataSource.initialize()
+.then(() => {
+ console.log('Database connected');
+})
+.catch(error => {
+ console.log(error)
+})
 
 app.listen(PORT, () => {
     console.log(`Server running ${PORT}`);
